43520c09f3fd63095d11327891c1f9d10b043a70
Richard Niemand <niemand.richard@gmail.com>
2025-06-19 15:39:54 -0600

    Updates metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-	title: "Eleventy Base Blog v9",
-	url: "https://example.com/",
+	title: "RichardN",
+	url: "https://www.richardn.ca/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Coding and more",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Richard Niemand",
+		email: "niemand.richard@gmail.com",
+		url: "https://www.richardn.ca/"
 	}
 }
