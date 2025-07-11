b56738fc7b3b58a4945fc7c1a2f82d12156787cc
Richard Niemand <rniemand@klick.com>
2025-07-11 10:59:56 -0600

    Reworking how tags are displayed

--- _config/filters.js
+++ _config/filters.js
@@ -1,37 +1,42 @@
-import { DateTime } from "luxon";
+import {DateTime} from "luxon";
 
 export default function (eleventyConfig) {
 	eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
 		// Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
-		return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(format || "dd LLLL yyyy");
+		return DateTime.fromJSDate(dateObj, {zone: zone || "utc"}).toFormat(format || "dd LLLL yyyy");
+	});
+
+	eleventyConfig.addFilter("readableDate2", (dateObj, format, zone) => {
+		// Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
+		return DateTime.fromJSDate(dateObj, {zone: zone || "utc"}).toFormat(format || "dd MMM yyyy");
 	});
 
 	eleventyConfig.addFilter("htmlDateString", (dateObj) => {
 		// dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-		return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat('yyyy-LL-dd');
+		return DateTime.fromJSDate(dateObj, {zone: "utc"}).toFormat("yyyy-LL-dd");
 	});
 
-	eleventyConfig.addFilter('stringify', (data) => {
-		return JSON.stringify(Object.keys(data), null, "\t")
+	eleventyConfig.addFilter("dumpKeys", (data) => {
+		return JSON.stringify(Object.keys(data), null, "\t");
 	});
 
-	eleventyConfig.addFilter('postBanner', (post, page) => {
-		let climbDirs = page.url.split('/').length - 1;
-		let finalPath = '';
-		for (let i = 0; i < climbDirs; i++) finalPath += '../';
-		return finalPath + 'img/post-banners/' + post.data.banner;
+	eleventyConfig.addFilter("postBanner", (post, page) => {
+		let climbDirs = page.url.split("/").length - 1;
+		let finalPath = "";
+		for (let i = 0; i < climbDirs; i++) finalPath += "../";
+		return finalPath + "img/post-banners/" + post.data.banner;
 	});
 
-	eleventyConfig.addFilter('pageSlug', (page) => {
-		let stripped = page.url.replaceAll('/', '_').replaceAll('-', '');
+	eleventyConfig.addFilter("pageSlug", (page) => {
+		let stripped = page.url.replaceAll("/", "_").replaceAll("-", "");
 		return stripped.substring(1, stripped.length - 1);
 	});
 
-	eleventyConfig.addFilter('postLogo', (post, page) => {
-		let climbDirs = page.url.split('/').length - 1;
-		let finalPath = '';
-		for (let i = 0; i < climbDirs; i++) finalPath += '../';
-		return finalPath + 'img/post-logos/' + post.data.logo;
+	eleventyConfig.addFilter("postLogo", (post, page) => {
+		let climbDirs = page.url.split("/").length - 1;
+		let finalPath = "";
+		for (let i = 0; i < climbDirs; i++) finalPath += "../";
+		return finalPath + "img/post-logos/" + post.data.logo;
 	});
 
 	// Get the first `n` elements of a collection.
@@ -52,15 +57,13 @@ export default function (eleventyConfig) {
 	});
 
 	// Return the keys used in an object
-	eleventyConfig.addFilter("getKeys", target => {
+	eleventyConfig.addFilter("getKeys", (target) => {
 		return Object.keys(target);
 	});
 
 	eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
-		return (tags || []).filter(tag => ["all", "posts"].indexOf(tag) === -1);
+		return (tags || []).filter((tag) => ["all", "posts"].indexOf(tag) === -1);
 	});
 
-	eleventyConfig.addFilter("sortAlphabetically", strings =>
-		(strings || []).sort((b, a) => b.localeCompare(a))
-	);
-};
+	eleventyConfig.addFilter("sortAlphabetically", (strings) => (strings || []).sort((b, a) => b.localeCompare(a)));
+}
