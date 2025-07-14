760101be2da19492b2795e7f1282de7c0adc10c4
rniemand <rniemand@users.noreply.github.com>
2025-07-14 14:22:57 +0000

    deploy: 0066142b39eb845e4945a196cbdcb3ec14427599

--- dist/FtW8cD1Sur.js
+++ dist/FtW8cD1Sur.js
@@ -250,13 +250,3 @@ const styleBlockquotes = () => {
 
 styleTables();
 styleBlockquotes();
-var disqus_config = function () {
-		this.page.url = '/blog/2016/2016-08-25/post2/';  // Replace PAGE_URL with your page's canonical URL variable
-		this.page.identifier = 'blog_2016_20160825_post2'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-    };
-    (function() { // DON'T EDIT BELOW THIS LINE
-    var d = document, s = d.createElement('script');
-    s.src = 'https://richardn-ca.disqus.com/embed.js';
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);
-    })();
